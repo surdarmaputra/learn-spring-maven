86aff142f38f082b4155299def5f0503d41e7d25
surdarmaputra <surya.igede@gmail.com>
2023-06-22 14:42:17 +0700

    init project

---
+++ src/main/java/com/example/learnspringmaven/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.learnspringmaven;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Hello fellas";
+  }
+  
+}
---
+++ src/test/java/com/example/learnspringmaven/LearnSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnspringmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
